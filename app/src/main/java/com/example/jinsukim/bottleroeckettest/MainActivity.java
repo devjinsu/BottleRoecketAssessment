88914b4c61c5b9bc0c35a8e57bcbb2f30fd62639
jinsu <devjinsu@gmail.com>
2018-06-30 13:38:00 -0500

    Able to save data. Show list of store Show detail

--- app/src/main/java/com/example/jinsukim/bottleroeckettest/MainActivity.java
+++ app/src/main/java/com/example/jinsukim/bottleroeckettest/MainActivity.java
@@ -1,149 +1,192 @@
 package com.example.jinsukim.bottleroeckettest;
 
+import android.Manifest;
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.example.jinsukim.bottleroeckettest.adapters.StoreAdapter;
+import com.example.jinsukim.bottleroeckettest.models.StoreModel;
 import com.example.jinsukim.bottleroeckettest.utils.Helpers;
+import com.example.jinsukim.bottleroeckettest.utils.RestTask;
+import com.example.jinsukim.bottleroeckettest.utils.SharePref;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends Activity {
+import cz.msebera.android.httpclient.client.methods.HttpGet;
 
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity implements StoreAdapter.OnStoreRecyclerViewListener {
+    private final String ACTION_FOR_INTENT_CALLBACK_STORE = "ACTION_FOR_INTENT_CALLBACK_STORE";
+    private static final int REQUEST_PERMISSION = 1;
+    private List<StoreModel> mStoreList;
+    private RecyclerView mStoreView;
+    private StoreAdapter mStoreAdapter;
+    private RelativeLayout mProgressLayout;
+    private int mUserLastSelOfPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        initUI();
+        requestData();
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //new JsonTask().execute(Helpers.getInstance().getAPIUrl());
+        registerReceiver(receiver, new IntentFilter(ACTION_FOR_INTENT_CALLBACK_STORE));
     }
 
 
-    private class JsonTask extends AsyncTask<String, String, String> {
-        ProgressDialog mProgressDialog;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
 
-        protected void onPreExecute() {
-            super.onPreExecute();
 
-            mProgressDialog = new ProgressDialog(MainActivity.this);
-            mProgressDialog.setMessage("Please wait");
-            mProgressDialog.setCancelable(false);
-            mProgressDialog.show();
+    private void initUI() {
+        mStoreView = (RecyclerView) findViewById(R.id.rv_store);
+        mStoreAdapter = new StoreAdapter(this, mStoreList);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mStoreView.setLayoutManager(layoutManager);
+
+        mStoreView.setAdapter(mStoreAdapter);
+
+        mProgressLayout = (RelativeLayout) findViewById(R.id.rl_progress);
+    }
+
+    private void initData() {
+        mStoreList = new ArrayList<>();
+        mUserLastSelOfPhone = 0;
+    }
+
+    private void requestData() {
+        try {
+            mProgressLayout.setVisibility(View.VISIBLE);
+            HttpGet httpGet = new HttpGet(new URI(Helpers.getInstance().getAPIUrl()));
+            RestTask task = new RestTask(this, ACTION_FOR_INTENT_CALLBACK_STORE);
+            task.execute(httpGet);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
         }
+    }
 
-        protected String doInBackground(String... params) {
-
-
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-
-                InputStream stream = connection.getInputStream();
-
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line+"\n");
-                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
-
-                }
-
-                return buffer.toString();
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (mProgressDialog.isShowing()){
-                mProgressDialog.dismiss();
+        public void onReceive(Context context, Intent intent) {
+            mProgressLayout.setVisibility(View.GONE);
+            String response = intent.getStringExtra(RestTask.HTTP_RESPONSE);
+            Log.i(TAG, "RESPONSE = " + response);
+            StoreParser(response);
+        }
+    };
+
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Thanks, Click again to call", Toast.LENGTH_SHORT).show();
+            } else {
+                // User refused to grant permission.
+                Toast.makeText(MainActivity.this, "Need to allow permission for calling", Toast.LENGTH_SHORT).show();
             }
-            Log.d(this.toString(), result);
         }
     }
 
-    public String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream is = getAssets().open("data.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
-    }
 
-    private void parseAndMakeModel(String ){
-        try {
-            JSONObject obj = new JSONObject(loadJSONFromAsset());
-            JSONArray m_jArry = obj.getJSONArray("formules");
-            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
-            HashMap<String, String> m_li;
-
-            for (int i = 0; i < m_jArry.length(); i++) {
-                JSONObject jo_inside = m_jArry.getJSONObject(i);
-                Log.d("Details-->", jo_inside.getString("formule"));
-                String formula_value = jo_inside.getString("formule");
-                String url_value = jo_inside.getString("url");
-
-                //Add your values in your `ArrayList` as below:
-                m_li = new HashMap<String, String>();
-                m_li.put("formule", formula_value);
-                m_li.put("url", url_value);
-
-                formList.add(m_li);
+    public void StoreParser(String rawData) {
+        JSONObject reader = null;
+        if (rawData.equals("")) {
+            rawData = SharePref.getInstance(MainActivity.this).getStoreData();
+            if (rawData.equals("")) {
+                Toast.makeText(MainActivity.this, "Not able to connect to server.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Showing result from cached data.", Toast.LENGTH_LONG).show();
             }
+        } else {
+            Toast.makeText(MainActivity.this, "Showing result from online.", Toast.LENGTH_LONG).show();
+            SharePref.getInstance(MainActivity.this).saveStoreData(rawData);
+        }
+        try {
+            reader = new JSONObject(rawData);
+            JSONArray results = reader.getJSONArray("stores");
+            Log.d(TAG, results.toString());
+
+            for (int i = 0; i < results.length(); i++) {
+                int id = results.getJSONObject(i).getInt("storeID");
+                String name = results.getJSONObject(i).getString("name");
+                String phone = results.getJSONObject(i).getString("phone");
+                String address = results.getJSONObject(i).getString("address");
+                String city = results.getJSONObject(i).getString("city");
+                String state = results.getJSONObject(i).getString("state");
+                String storeLogoURL = results.getJSONObject(i).getString("storeLogoURL");
+                String zipcode = results.getJSONObject(i).getString("zipcode");
+                double latitude = results.getJSONObject(i).getDouble("latitude");
+                double longitude = results.getJSONObject(i).getDouble("longitude");
+
+                StoreModel store = new StoreModel(id, address, city, state, zipcode, name, phone, latitude, longitude, storeLogoURL);
+                mStoreList.add(store);
+            }
+
+            mStoreAdapter.notifyDataSetChanged();
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onStoreItemClickEvent(int position) {
+        Intent intent = new Intent(MainActivity.this, StoreDetailActivity.class);
+        intent.putExtra(StoreDetailActivity.EXTRA_STORE_DETAIL, mStoreList.get(position));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStoreCallClickEvent(int position) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},
+                    REQUEST_PERMISSION);
+
+            return;
+
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + mStoreList.get(position).getPhone()));
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/example/jinsukim/bottleroeckettest/adapters/StoreAdapter.java
+++ app/src/main/java/com/example/jinsukim/bottleroeckettest/adapters/StoreAdapter.java
@@ -1,6 +1,8 @@
 package com.example.jinsukim.bottleroeckettest.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,24 +19,28 @@ import java.util.List;
 public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder> {
     private Context mContext;
     private List<StoreModel> mStoreList;
+    private OnStoreRecyclerViewListener mListener;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public ImageView thumbnail;
         public TextView address, city, state, phone;
+        public View view;
 
-        public MyViewHolder(View view){
+        public MyViewHolder(View view) {
             super(view);
             thumbnail = (ImageView) view.findViewById(R.id.iv_thumbnail);
             address = (TextView) view.findViewById(R.id.address);
             city = (TextView) view.findViewById(R.id.city);
             state = (TextView) view.findViewById(R.id.state);
             phone = (TextView) view.findViewById(R.id.phone);
+            this.view = view;
         }
     }
 
-    public StoreAdapter(Context context, List<StoreModel> storeList){
+    public StoreAdapter(Context context, List<StoreModel> storeList) {
         this.mContext = context;
         this.mStoreList = storeList;
+        this.mListener = (OnStoreRecyclerViewListener) mContext;
     }
 
     @Override
@@ -44,12 +50,28 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
         StoreModel store = mStoreList.get(position);
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onStoreItemClickEvent(position);
+            }
+        });
         holder.address.setText(store.getAddress());
         holder.city.setText(store.getCity());
-        holder.state.setText(store.getState());
+        holder.state.setText(", " + store.getState());
         holder.phone.setText(store.getPhone());
+        holder.phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onStoreCallClickEvent(position);
+//                Intent intent = new Intent(Intent.ACTION_CALL);
+//
+//                intent.setData(Uri.parse("tel:" + holder.phone.getText().toString()));
+//                mContext.startActivity(intent);
+            }
+        });
 
         //load thumbnail from url
         Glide.with(mContext).load(store.getLogoURL()).into(holder.thumbnail);
@@ -58,6 +80,11 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.MyViewHolder
     @Override
     public int getItemCount() {
         return mStoreList.size();
+    }
+
+    public interface OnStoreRecyclerViewListener {
+        void onStoreItemClickEvent(int position);
+        void onStoreCallClickEvent(int position);
     }
 
 }
